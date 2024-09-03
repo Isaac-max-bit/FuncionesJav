34f001a00e88bda54ea0f1c77b31dcefa1e97115
Brayan_Giraldo <giraldo8900@gmail.com>
2024-09-03 08:17:46 -0500

    propiety

---
+++ script.js
@@ -0,0 +1,34 @@
+/* Funcion regular */
+
+// function unaFuncion() {
+//     for (let index = 0; index < 5; index++){
+    
+//     console.log(25 * 25);
+//     console.log(25 - 25);
+//     console.log(25 / 25);
+//     console.log(25 + 25);
+//     console.log("Vuelta: " + index);
+// }
+// }
+
+/* Función Regular - Llamado  */
+
+// unaFuncion();
+// let nombre = 'Jaime Zapata'
+// console.log(nombre);
+
+// let nombre = prompt("Ingresa tu nombre  : ")
+// alert("Que onda " + nombre)
+
+funcionDeExpresion()
+let funcionDeExpresion = function(){
+    for (let index = 0; index < 5; index++){
+    
+        console.log(25 * 25);
+        console.log(25 - 25);
+        console.log(25 / 25);
+        console.log(25 + 25);
+        console.log("Vuelta: " + index);
+}
+
+};
